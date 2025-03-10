e623eadb953eadce88b034d6eb97d35113310791
Miguel-Andrade-Cruz <angelium.eddra1972@gmail.com>
2025-03-10 18:26:05 -0300

    Rearrange routes to better domain rules

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AssayController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -8,8 +9,18 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::prefix('auth')->group( function () {
+Route::prefix('auth')->group(function () {
     Route::post('/login', [AuthController::class, 'login']);
-    Route::post('/register', [AuthController::class, 'register']);
-    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/register', [AuthController::class, 'register'])->middleware('auth:sanctum');
+    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+    Route::get('/me', [AuthController::class, 'me'])->middleware('auth:sanctum');
 });
+
+
+
+Route::prefix('assay')->group(function () {  
+    Route::get('/get{query}', [AssayController::class, 'getAssays']);
+    Route::post('/new', [AssayController::class, 'newAssay']);
+    Route::patch('/write', [AssayController::class, 'rewriteAssay']);
+    Route::delete('/erase', [AssayController::class, 'eraseAssay']);
+})->middleware('auth:sanctum');
