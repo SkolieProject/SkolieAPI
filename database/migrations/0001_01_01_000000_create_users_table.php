cba71b6cc81d1200665cb895d768840823569317
Miguel-Andrade-Cruz <angelium.eddra1972@gmail.com>
2025-03-15 18:18:33 -0300

    add admin role to the database

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->enum('role', ['STDNT', 'TCHR']);
+            $table->enum('role', ['STDNT', 'TCHR', 'ADMIN']);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
